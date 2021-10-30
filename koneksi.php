6bb93db3dad8328f5fa0d18b7e0ee7c053314238
devipuspitaaa <puspitasaridevi228@gmail.com>
2021-10-30 17:02:17 +0700

    push

--- koneksi.php
+++ koneksi.php
@@ -2,7 +2,7 @@
 $db_host = "10.0.0.40";
 $db_user = "admin";
 $db_pass = "Devi123;";
-$db_name = "Tokol"; 
+$db_name = "Tokol";  
 
 $koneksi = mysqli_connect($db_host, $db_user, $db_pass, $db_name);
 
